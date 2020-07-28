2a6bfd990348e82ed80182553a0e158d6b304c4c
SimaxSaab <minorea@yandex.ru>
2020-07-28 23:08:58 +0300

    Updates

---
+++ precache-manifest.24dd4db5141545811e49829dd14bb595.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6feb2c45e79ea1846935b63a5cad592",
+    "url": "/todoWithReact/index.html"
+  },
+  {
+    "revision": "81c971c18009ec17cd63",
+    "url": "/todoWithReact/static/css/main.ecf33a3d.chunk.css"
+  },
+  {
+    "revision": "3d4e1ddba399fd9b6632",
+    "url": "/todoWithReact/static/js/2.8617e881.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoWithReact/static/js/2.8617e881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c971c18009ec17cd63",
+    "url": "/todoWithReact/static/js/main.4fdf1a26.chunk.js"
+  },
+  {
+    "revision": "38b7d6dc98668c4ce810",
+    "url": "/todoWithReact/static/js/runtime-main.ef410a59.js"
+  }
+]);
